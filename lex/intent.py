603554b4ac75e9b6e2bcec67b584fac75776f9d1
Troy Larson <troylar@gmail.com>
2018-09-09 19:47:00 -0400

    Intent code done, no testing

--- lex/intent.py
+++ lex/intent.py
@@ -28,6 +28,17 @@ class WithMessage:
         return self
 
 
+class DialogHookCode:
+    def __init__(self, **kwargs):
+        self.uri = kwargs.get('Uri')
+        self.messageVersion = kwargs.get('MessageVersion')
+
+
+class FulfillmentActivity:
+    def __init__(self, **kwargs):
+        self.type = kwargs.get('Type')
+        self.code_hook = kwargs.get('CodeHook')
+
 class ConfirmationPrompt(WithMessage):
     def __init__(self, **kwargs):
         super(ConfirmationPrompt, self).__init__(**kwargs)
@@ -126,6 +137,12 @@ class Intent:
         self.create_version = kwargs.get('CreateVersion', False)
         self.value_selection_strategy = kwargs.get('ValueSelectionStrategy', 'ORIGINAL_VALUE')
         self.synonyms = kwargs.get('Synonyms')
+        self.confirmation_prompt = kwargs.get('ConfirmationPrompt', {})
+        self.rejection_statement = kwargs.get('RejectionStatement', {})
+        self.follow_up_prompt = kwargs.get('FollowUpPrompt', {})
+        self.parentIntentSignature = kwargs.get('ParentIntentSignature')
+        self.checksum = kwargs.get('Checksum')
+        self.create_version = kwargs.get('CreateVersion', False)
 
     def with_name(self, name):
         self.name = name
@@ -154,6 +171,27 @@ class Intent:
     def with_value_selection_strategy(self, strategy):
         self.value_selection_strategy = strategy
         return self
+
+    def with_confirmation_prompt(self, confirmation_prompt):
+        self.confirmation_prompt = confirmation_prompt
+        return self
+
+    def with_rejection_statement(self, rejection_statement):
+        self.rejection_statement = rejection_statement
+        return self
+
+    def with_follow_up_prompt(self, follow_up_prompt):
+        self.follow_up_prompt = follow_up_prompt
+        return self
+
+    def with_conclusion_statement(self, conclusion_statement):
+        self.conclusion_statement = conclusion_statement
+        return self
+
+    def with_dialog_hook(self, dialog_hook):
+        self.dialog_hook = dialog_hook
+        return self
+
 
     def apply(self):
         client = boto3.client('lex-models')
